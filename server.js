be8015fbd46ea7f6f4347b79c0aa65334ebc3bac
Sinceronico <140562682+sinceronico@users.noreply.github.com>
2026-09-07 21:44:29 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,18 @@
+tiktokLiveConnection = new WebcastPushConnection(activeStreamer, {
+    processInitialData: false,
+    enableExtendedGiftInfo: true,
+    requestPollingIntervalMs: 1000,
+    clientParams: {
+      "app_language": "pt-BR",
+      "device_platform": "web",
+      "webcast_language": "pt-BR"
+    },
+    requestOptions: {
+      headers: {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36"
+      }
+    }
+  });
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
